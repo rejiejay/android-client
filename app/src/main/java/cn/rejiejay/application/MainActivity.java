2715179003d40fc3d68e03f029e62abaa5f622f1
rejiejay <454766952@qq.com>
2019-07-30 22:23:33 +0800

    完成 tab 事件绑定

--- app/src/main/java/cn/rejiejay/application/MainActivity.java
+++ app/src/main/java/cn/rejiejay/application/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -23,8 +24,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 顶部按钮
      */
-    public ActivityTabButton homeTab;
-    public ActivityTabButton recordTab;
+    public ArrayList<ActivityTabButton> tabBtnList;
 
     /**
      * ViewPager 分页组件
@@ -46,11 +46,15 @@ public class MainActivity extends AppCompatActivity {
      * 初始化 绑定 组件的方法
      */
     public void initPageComponent() {
-        homeTab = findViewById(R.id.ActivityTabHome);
-        recordTab = findViewById(R.id.ActivityTabRecord);
+        tabBtnList = new ArrayList<>();
+        ActivityTabButton activityTabHome = findViewById(R.id.ActivityTabHome);
+        ActivityTabButton activityTabRecord = findViewById(R.id.ActivityTabRecord);
 
-        homeTab.setTextContentAndAlpha(true, "首页");
-        recordTab.setTextContentAndAlpha(false, "记录");
+        tabBtnList.add(activityTabHome);
+        tabBtnList.add(activityTabRecord);
+
+        activityTabHome.setTextContentAndAlpha(true, "首页");
+        activityTabRecord.setTextContentAndAlpha(false, "记录");
 
         myViewPager = findViewById(R.id.MyViewPager);
     }
@@ -63,11 +67,11 @@ public class MainActivity extends AppCompatActivity {
          * PagerAdapter 如何使用?
          * PagerAdapter 是 android.support.v4包中的类，它的子类有FragmentPagerAdapter, FragmentStatePagerAdapter，这两个adapter都是Fragment的适配器，用于实现Fragment的滑动效果
          */
-        ArrayList<Fragment> list = new ArrayList<>();
-        list.add(new HomeFragment()); // 添加Fragment到集合里设置setAdapter适配器
-        list.add(new RecordFragment());
+        ArrayList<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new HomeFragment()); // 添加Fragment到集合里设置setAdapter适配器
+        fragmentList.add(new RecordFragment());
 
-        PagerAdapter myPagerAdapter = new MainFragmentAdapter(getSupportFragmentManager(), list);
+        PagerAdapter myPagerAdapter = new MainFragmentAdapter(getSupportFragmentManager(), fragmentList);
 
         /**
          * 为 ViewPager 设置 Adapter 适配器
@@ -75,23 +79,51 @@ public class MainActivity extends AppCompatActivity {
         myViewPager.setAdapter(myPagerAdapter);
 
         /**
-         *
+         * 监听页面改变的事件
          */
         myViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) { }
 
-            }
-
+            /**
+             * 设置导航按钮显示和激活
+             *
+             * @param position 当前选中的视图
+             */
             @Override
             public void onPageSelected(int position) {
-//                selectedTab(position);
+                /**
+                 * 先执行一遍清空操作
+                 */
+                for (int i = 0; i < tabBtnList.size(); i++) {
+                    tabBtnList.get(i).setSelected(false);
+                }
+
+                /**
+                 * 再根据选中视图的下标设置激活
+                 */
+                tabBtnList.get(position).setSelected(true);
             }
 
             @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
+            public void onPageScrollStateChanged(int state) { }
         });
+
+        /**
+         * 绑定 Tab 的点击事件
+         */
+        for (int i = 0; i < tabBtnList.size(); i++) {
+            ActivityTabButton thisTabBtnList = tabBtnList.get(i);
+
+            thisTabBtnList.setTag(i); // 保存当前的索引值
+
+            thisTabBtnList.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View thisView) {
+                    // 根据上面的索引值 修改页面
+                    myViewPager.setCurrentItem((Integer) thisView.getTag());
+                }
+            });
+        }
     }
 }

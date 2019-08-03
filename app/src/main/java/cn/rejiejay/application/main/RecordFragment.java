6431a9b6cb5b08b71ef266087b157d4e435cbc60
rejiejay <454766952@qq.com>
2019-08-03 20:25:59 +0800

    add 问题：加载网络图片失败的问题

--- app/src/main/java/cn/rejiejay/application/main/RecordFragment.java
+++ app/src/main/java/cn/rejiejay/application/main/RecordFragment.java
@@ -1,5 +1,7 @@
 package cn.rejiejay.application.main;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,6 +9,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import cn.rejiejay.application.R;
 
@@ -18,6 +27,58 @@ public class RecordFragment extends Fragment {
          * 绑定 layout
          */
         return inflater.inflate(R.layout.activity_main_fragment_record, container, false);
+    }
+
+    /**
+     * 这里加载图片会报错
+     */
+    // @Override
+    // public void onCreate(@Nullable Bundle savedInstanceState) {
+    //     super.onCreate(savedInstanceState);
+    // }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        ImageView myImage = view.findViewById(R.id.record_Item_img);
+//
+//        Bitmap bitmap = getImageBitmap("https://rejiejay-1251940173.cos.ap-guangzhou.myqcloud.com/myweb/mobile-list/articles-1.png");
+//
+//        myImage.setImageBitmap(bitmap); // 设置Bitmap
+    }
+
+    /**
+     * Android之网络图片加载的5种基本方式
+     * https://blog.csdn.net/DickyQie/article/details/59146221
+     *
+     * @param url
+     * @return
+     */
+    public Bitmap getImageBitmap(String url) {
+        Bitmap bitmap = null;
+
+        try {
+            URL imgUrl = new URL(url);
+
+            HttpURLConnection conn = (HttpURLConnection) imgUrl
+                    .openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+        return bitmap;
     }
 }
 

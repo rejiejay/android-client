04da4ee56091d27a7cf15ffc89ee3c4f2b533f79
rejiejay <454766952@qq.com>
2019-08-18 08:44:48 +0800

    瞎几把更新一波，准备分文件夹

--- app/src/main/java/cn/rejiejay/application/RecordEventActivity.java
+++ app/src/main/java/cn/rejiejay/application/RecordEventActivity.java
@@ -1,14 +1,24 @@
 package cn.rejiejay.application;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 import com.qmuiteam.qmui.widget.textview.QMUISpanTouchFixTextView;
 
+import java.util.ArrayList;
+
+import cn.rejiejay.application.recordevent.UploadImageAdapter;
+
 public class RecordEventActivity extends AppCompatActivity {
+    private Context mContext;
+    private ArrayList<String> mPicList = new ArrayList<>(); //上传的图片凭证的数据源
+
     /**
      * 页面状态
      * record 记录页面 event 事件页面
@@ -24,6 +34,8 @@ public class RecordEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_event);
 
+        mContext = this;
+
         // 取得从上一个Activity当中传递过来的Intent对象
         Intent intent = getIntent();
         //从Intent当中根据key取得value
@@ -35,6 +47,8 @@ public class RecordEventActivity extends AppCompatActivity {
 
         initPageType(); // 根据 pageType 改变页面状态
         initTopTab(); // 初始化 顶部
+
+        initGridView(); // 初始化展示上传图片的GridView
     }
 
     /**
@@ -86,6 +100,44 @@ public class RecordEventActivity extends AppCompatActivity {
                 initPageType();
             }
         });
+    }
 
+    // 初始化展示上传图片的GridView
+    private void initGridView() {
+        GridView gridView = findViewById(R.id.record_event_grid);
+        UploadImageAdapter mGridViewAddImgAdapter = new UploadImageAdapter(mContext, mPicList);
+
+        gridView.setAdapter(mGridViewAddImgAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                if (position == parent.getChildCount() - 1) {
+                    // 如果“增加按钮形状的”图片的位置是最后一张，且添加了的图片的数量不超过1张，才能点击
+                    if (mPicList.size() == 1) {
+                        // 点击图片查看大图（最多添加1张图片
+//                        viewPluImg(position);
+                    } else {
+                        // 添加凭证图片
+//                        selectPic(MainConstant.MAX_SELECT_PIC_NUM - mPicList.size());
+                    }
+                } else {
+                    // 点击图片查看大图
+//                    viewPluImg(position);
+                }
+            }
+        });
+    }
+
+    /**
+     * 查看大图
+     *
+     * @param position
+     */
+    private void viewPluImg(int position) {
+//        Intent intent = new Intent(mContext, PlusImageActivity.class);
+//        intent.putStringArrayListExtra(MainConstant.IMG_LIST, mPicList);
+//        intent.putExtra(MainConstant.POSITION, position);
+//        startActivityForResult(intent, MainConstant.REQUEST_CODE_MAIN);
     }
 }
